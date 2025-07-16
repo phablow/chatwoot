557a040e4bd20d24cd1cc30fdbd28bdaf33ace99
phablow <37158165+phablow@users.noreply.github.com>
2025-07-16 16:02:43 -0300

    Update default-sidebar.js
    Atualização perfil de agente

--- app/javascript/dashboard/components/layout/config/default-sidebar.js
+++ app/javascript/dashboard/components/layout/config/default-sidebar.js
@@ -1,5 +1,4 @@
 import conversations from './sidebarItems/conversations';
-import contacts from './sidebarItems/contacts';
 import reports from './sidebarItems/reports';
 import campaigns from './sidebarItems/campaigns';
 import settings from './sidebarItems/settings';
@@ -10,7 +9,6 @@ export const getSidebarItems = accountId => ({
   primaryMenu: primaryMenu(accountId),
   secondaryMenu: [
     conversations(accountId),
-    contacts(accountId),
     reports(accountId),
     campaigns(accountId),
     settings(accountId),
